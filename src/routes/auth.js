c3c16de759ca26b998cf339b06e3ba8ddd8cff48
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2021-01-21 12:35:14 -0600

    Se validaron las rutas de auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,28 @@
 // host + /api/auth
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { createUser, loginUser, revalidateToken } = require('../controllers/authController');
+const { validateFields } = require('../middlewares/validateFields');
 const router = Router();
 
 router.post(
     '/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe tener al menos 6 carácteres').isLength({ min: 6 }),
+        validateFields
+    ],
     createUser
 );
 
 router.post(
     '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe tener al menos 6 carácteres').isLength({ min: 6 }),
+        validateFields
+    ],
     loginUser
 );
 
